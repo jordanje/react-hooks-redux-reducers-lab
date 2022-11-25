fdbd840d943f7d7dcf713deb0dafe49118394a4d
Jordan Jennings <jordanjennings12@gmail.com>
2022-11-25 12:48:38 -0500

    tests completed

--- src/managePresents.js
+++ src/managePresents.js
@@ -1,5 +1,14 @@
 const initialState = { numberOfPresents: 0 };
 
 export function managePresents(state = initialState, action) {
-  // your code here
+  // if(action.type = "presents/increase"){
+  //   state = { numberOfPresents: state.numberOfPresents + 1 }
+  // }
+  // return state
+  switch (action.type) {
+    case 'presents/increase':
+      return { numberOfPresents: state.numberOfPresents + 1}
+    default:
+      return state
+  }
 }
